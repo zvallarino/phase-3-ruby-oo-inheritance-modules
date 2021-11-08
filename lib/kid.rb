65b193af3c659a47b86dbb1c976e5ec9296f680c
Zachary <zvcoding@gmail.com>
2021-11-07 19:46:21 -0500

    commit tree 947ca4c7ee3f68b92a3c33e2192625d389750677

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,16 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Kid
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
---
+++ lib/meta_dancing_module.rb
@@ -0,0 +1,7 @@
+module MetaDancing
+
+  def metadata
+    "This class produces objects that love to dance."
+  end 
+
+end 
